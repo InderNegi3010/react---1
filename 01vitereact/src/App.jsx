53f0097d888d2a88c8c1fc2f1ad7da14395dccdc
InderNegi3010 <snegi1070@gmail.com>
2025-06-03 09:36:50 +0530

    using react and vite

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+
+
+function App() {
+
+  return (
+    <>
+        <h2>Inder bro is here</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste vero vitae sequi.</p>
+   </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,16 @@
+
+
+function Chai() {
+  return (
+    <div>
+      <h1>Chai</h1>
+      <p>
+        Chai is a popular assertion library for Node.js and browsers, often used with testing frameworks like Mocha. It provides a variety of assertion styles, including BDD (Behavior-Driven Development) and TDD (Test-Driven Development).
+      </p>
+      <p>
+        Chai allows developers to write tests in a more expressive and readable manner, making it easier to understand the intent of the tests.
+      </p>
+    </div>
+  );
+}
+export default Chai;
